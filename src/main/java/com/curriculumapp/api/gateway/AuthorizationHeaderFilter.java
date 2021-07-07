575c11ea7defced2681bb8a97d1f448adacc5441
Ervert <e.condori.v@gmail.com>
2021-07-07 18:37:39 -0500

    gateway

---
+++ src/main/java/com/curriculumapp/api/gateway/AuthorizationHeaderFilter.java
@@ -0,0 +1,63 @@
+package com.curriculumapp.api.gateway;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+
+import io.jsonwebtoken.Jwts;
+import reactor.core.publisher.Mono;
+@Component
+public class AuthorizationHeaderFilter extends AbstractGatewayFilterFactory<AuthorizationHeaderFilter.Config> {
+	@Autowired	
+	private Environment env;
+	public AuthorizationHeaderFilter() {
+		super(Config.class);
+	}
+	
+	public static class Config{
+		
+	}
+
+	@Override
+	public GatewayFilter apply(Config config) {
+		return (exchange,chain)->{
+			ServerHttpRequest request = exchange.getRequest();
+			if(!request.getHeaders().containsKey(HttpHeaders.AUTHORIZATION)) {
+				return onError(exchange,"No Authorization header", HttpStatus.UNAUTHORIZED);
+			}
+			String authorizationHeader = request.getHeaders().get(HttpHeaders.AUTHORIZATION).get(0);
+			String jwt = authorizationHeader.replace("Bearer", "");
+			if(!isJwtvalid(jwt)) {
+				return onError(exchange,"token is not valid", HttpStatus.UNAUTHORIZED);
+			}
+			return chain.filter(exchange);	
+		};
+	}
+
+	private Mono<Void> onError(ServerWebExchange exchange, String string, HttpStatus unauthorized) {
+		ServerHttpResponse response = exchange.getResponse();
+		response.setStatusCode(unauthorized);
+		return response.setComplete();
+	}
+	
+	private boolean isJwtvalid(String jwt) {
+		boolean returnValue = true;
+		String subject=Jwts.parser().setSigningKey(env.getProperty("token.secret"))
+		.parseClaimsJws(jwt)
+		.getBody()
+		.getSubject();
+		if(subject==null|| subject.isEmpty()) {
+			returnValue=false;
+		}
+		return returnValue;
+	}
+
+}
---
+++ src/main/java/com/curriculumapp/api/gateway/CurriculumAppApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.curriculumapp.api.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurriculumAppApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurriculumAppApiGatewayApplication.class, args);
+	}
+
+}
